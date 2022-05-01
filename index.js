b2ad5f24d6b7259da3bf76f3c8538ea6c08358d4
ae95medina <ae95medina@gmail.com>
2022-05-01 12:50:06 -0500

    commit tree 54c82fb9f5a14a9a88cc7102db970f39f50bdcbd

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+let num3 = 10
+let random = Math.ceil(Math.random() * num3)
+console.log(random)
+let num4 = 26
+let mod = num3 % 6
+let max = Math.max(1, 5, 10, 20)
